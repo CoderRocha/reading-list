77fadb7a64363e1188bf4130ddfcd63aff3ff279
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-20 10:39:02 -0300

    added 'Firebase v9' to the project

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAKLBOOt6CvycLnTP6QaN4CGDAl29JLUGE",
+    authDomain: "reading-list-web-app.firebaseapp.com",
+    projectId: "reading-list-web-app",
+    storageBucket: "reading-list-web-app.appspot.com",
+    messagingSenderId: "518776255900",
+    appId: "1:518776255900:web:304d08fd41722eba3b8459"
+};
