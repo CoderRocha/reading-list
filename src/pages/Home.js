9a19ebdf5cfdca375599868178cdd8349a1d0ab5
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-20 10:50:38 -0300

    added logic to fetch data from firebase database

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
 
+import { db } from '../firebase/config'
+import { collection, getDocs } from 'firebase/firestore'
+
 export default function Home() {
-  const [books, setBooks] = useState([
-    { title: 'the diary of a wimp kid', id: 1 },
-    { title: 'the diary of a wimp kid 2', id: 2 },
-    { title: 'the diary of a wimp kid 3', id: 3 },
-    { title: 'the diary of a wimp kid 4', id: 4 }
-  ])
+  const [books, setBooks] = useState(null)
+
+  useEffect(() => {
+    const ref = collection(db, 'books')
+
+    getDocs(ref)
+      .then((snapshot) => {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({id: doc.id, ...doc.data()})
+        })
+        setBooks(results)
+      })
+  }, [])
 
   return (
     <div>
