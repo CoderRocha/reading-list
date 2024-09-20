de3e2994b15ab69ddec213414f858300985cebc2
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-20 10:28:54 -0300

    updated books names

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,10 @@ import BookForm from '../components/BookForm'
 
 export default function Home() {
   const [books, setBooks] = useState([
-    { title: 'the name of the wind', id: 1 },
-    { title: 'the dragon reborn', id: 2 },
-    { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 }
+    { title: 'the diary of a wimp kid', id: 1 },
+    { title: 'the diary of a wimp kid 2', id: 2 },
+    { title: 'the diary of a wimp kid 3', id: 3 },
+    { title: 'the diary of a wimp kid 4', id: 4 }
   ])
 
   return (
