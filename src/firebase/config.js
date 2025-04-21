9b564fd7c1cf2108fbefd509f8355cdcf0641fb5
Guilherme <guilhermerochas0420@gmail.com>
2025-04-20 22:18:05 -0300

    updated firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKLBOOt6CvycLnTP6QaN4CGDAl29JLUGE",
-    authDomain: "reading-list-web-app.firebaseapp.com",
-    projectId: "reading-list-web-app",
-    storageBucket: "reading-list-web-app.appspot.com",
-    messagingSenderId: "518776255900",
-    appId: "1:518776255900:web:304d08fd41722eba3b8459"
+    apiKey: PROCESS.env.apiKey,
+    authDomain: PROCESS.env.authDomain,
+    projectId: PROCESS.env.projectId,
+    storageBucket: PROCESS.env.storageBucket,
+    messagingSenderId: PROCESS.env.messagingSenderId,
+    appId: PROCESS.env.appId,
 };
 
 // initialize firebase
